c9ee9dacd67c117269d82e36297a10759bbb1cb5
Quiana Berry <berry.quiana@gmail.com>
2021-01-21 23:50:47 -0500

    trial 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+//Step 1 import everything you need + incl UseState
+import "./App.css"; //css for the app index.css is css for an individual component
+import info from "./components/Records.js"; //import data u need
+import { useState } from "react";// standard protocol when utilizing use state
+import Receipt from "components/Receipt";
 
+//Step 2
+//Add the receipt to the state of the app 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const [receipts, setReceipts] = useState(receipts)// use state returns current state and  function it updates = useState(0) 
 
+
+//Step 3 
+//Make a receipt component that renders the first reciepts
+return (
+  <div className="App-container">
+    {receipts.map((personalDish, index) => {
+      return <Receipt {...personalDish} key={index} />;
+    })}
+  </div>
+);
+}
 export default App;
---
+++ src/components/Receipt.js
@@ -0,0 +1,22 @@
+import "./Receipt.css"
+
+const Receipt = ({person, ...data}) => {
+    return (
+        <div className="receipt-container">
+            <h3> Order for : {(person).toUpperCase()}</h3>
+            <ul>
+                <li><span class="receipt-titles">Main:</span> {data.order.main}</li>
+                <li><span class="receipt-titles">Protein:</span> {data.order.protein}</li>
+                <li><span class="receipt-titles">Rice:</span> {data.order.rice}</li>
+                <li><span class="receipt-titles">Sauce:</span> {data.order.sauce}</li>
+                <li><span class="receipt-titles">Drink:</span> {data.order.drink}</li>
+
+               
+                <li class="cost"><span class="receipt-titles cost">Cost:</span>$ <span class="cost-amount"> {data.order.cost}</span></li>
+            </ul>
+        </div>
+    )
+}
+
+
+export default Receipt;
